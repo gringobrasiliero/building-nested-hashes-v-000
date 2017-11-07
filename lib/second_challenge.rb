1ce02e5ee875057752bf02224dccc307ab2fe2af
Nolan Byrnes <nolanehs@gmail.com>
2017-11-07 08:23:35 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends = []
+      :hero_friends => []
       #your key/value pairs here!
       },
     :capulet => {
@@ -12,7 +12,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends = []
+      :heroine_friends => []
       }}
 
 
